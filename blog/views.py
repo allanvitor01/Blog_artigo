cbc2931be02db060800aa626e9887025bbea3b00
Allan Carneiro <allanvitor.carneiro@gmail.com>
2018-06-30 17:10:53 -0300

    Inicial

---
+++ blog/views.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.db'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
+
+
+
+from .models import Paper
+from .models import Author
+from .models import Category
+
+@app.route("/")
+def pagina_home():
+
+    artigo = Paper.query.all()
+    autor = Author.examples()#Category.query.all()#
+    id_autor = Author.id#Category.query.all()#
+    #categoria = Category.examples()
+        #Category.query.all()
+
+    return render_template("index.html", artigo=artigo, autor=artigo, categoria=artigo, id01=id_autor)
+
+
+
+@app.route("/blog/")
+def pagina_blog_geral():
+
+    artigo = Paper.examples()
+
+    return render_template("blog_geral.html", artigo_exibir=artigo)
+
+
+
+@app.route("/blog/<slug>")
+def pagina_blog(slug):
+
+    artigo = Paper.query.filter_by(slug=slug)
+    tamanho = len(Paper.query.all())
+
+    for i in range(tamanho):
+        if artigo[i].slug == slug:
+            return render_template("blog.html", artigo_exibir=artigo[0])
+
+
+
